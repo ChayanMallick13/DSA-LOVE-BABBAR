54eab6e8d06a3914e87222fedd0d2fbcb107f43e
Chayan_Mallick_01 <141207003+ChayanMallick13@users.noreply.github.com>
2024-06-05 03:15:57 +0530

    Update FLATTEN_BST_SORTED.cpp

--- LECTURE - 71/FLATTEN_BST_SORTED.cpp
+++ LECTURE - 71/FLATTEN_BST_SORTED.cpp
@@ -1,3 +1,43 @@
 /*
   QUESTION LINK : -   https://www.naukri.com/code360/problems/flatten-bst-to-a-sorted-list_1169459?leftPanelTab=0
 */
+
+
+#include <bits/stdc++.h> 
+
+//inorder vector me store karo
+void get_in(TreeNode<int>* root,vector<int> & in){
+
+    //base 
+    if(root==NULL)
+        return ;
+
+    //L
+    get_in(root->left, in);
+
+    // N
+    in.push_back(root->data);
+
+    //R
+    get_in(root->right, in);
+}
+TreeNode<int>* flatten(TreeNode<int>* root)
+{
+    vector<int> in;
+    get_in(root, in);
+
+    //dummy head
+    TreeNode<int> * dum = new TreeNode<int>(-1),*tail = dum;
+
+    // in order sorted hota hain to vector se har element uthake new ll return kardo
+    for(int i = 0;i<in.size();i++){
+        TreeNode<int> * temp = new TreeNode<int>(in[i]);
+        temp->left = NULL;
+        temp->right = NULL;
+        tail->right=temp;
+        tail=temp;
+    }
+
+    
+    return dum->right;
+}
