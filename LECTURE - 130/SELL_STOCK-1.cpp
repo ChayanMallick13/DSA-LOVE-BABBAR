c10d4b99c6f863e8dbffa9eca2da1e3d522d1953
Chayan_Mallick_01 <141207003+ChayanMallick13@users.noreply.github.com>
2024-07-18 13:30:41 +0530

    Create SELL_STOCK-1.cpp

---
+++ LECTURE - 130/SELL_STOCK-1.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxi = prices[n-1];
+        int ans = 0;
+        for(int i = n-2;i>=0;i--){
+            ans = max(ans,maxi - prices[i]);
+            maxi = max(maxi,prices[i]);
+        }
+        return ans;
+    }
+};
